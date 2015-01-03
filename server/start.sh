24d0b5fbc3d9f2b17ba567f75700b2436382bb80
顾翔 <c0710204@gmail.com>
2015-01-03 20:15:52 +0800

    fix

--- server/start.sh
+++ server/start.sh
@@ -1,1 +1,1 @@
-(java -Xincgc -Xms1G -Xmx1G -Xss1M -server -jar cauldron-1.7.10-1.1207.01.187-server.jar &)
+nohup "java -Xincgc -Xms1G -Xmx1G -Xss1M -server -jar cauldron-1.7.10-1.1207.01.187-server.jar" &
